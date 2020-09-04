b144a5ea5a2b3318676ccf44c7971d8a93959187
Alvin <alvinleung2009@gmail.com>
2020-09-03 17:41:53 -0700

    Changed textfield style for better usability

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,32 +1,52 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Input = ({ name, children, placeholder, ...rest }) => (
-   <Label htmlFor={name}>
-      <ActualInput name={name} id={name} placeholder={placeholder || 'Type here'} {...rest} />
+  <Wrapper>
+    <Label htmlFor={name}>
       <span>{children}</span>
-   </Label>
-)
+      <ActualInput
+        name={name}
+        id={name}
+        placeholder={placeholder || "Type here"}
+        {...rest}
+      />
+    </Label>
+  </Wrapper>
+);
 
-export default Input
+export default Input;
+
+const Wrapper = styled.div`
+  background-color: #eee;
+  border-radius: 10px;
+  padding: 1rem 1rem 1rem;
+  margin-bottom: 1rem;
+
+  &:focus-within {
+    background-color: #1a213a;
+    color: #fff;
+  }
+`;
 
 const Label = styled.label`
-   span {
-      display: block;
-      font-size: 14px;
-      opacity: 0.5;
-      color: inherit;
-      font-family: inherit;
-      margin-bottom: 10px;
-   }
-`
+  span {
+    display: block;
+    font-size: 14px;
+    opacity: 0.5;
+    color: inherit;
+    font-family: inherit;
+    //  margin-bottom: 10px;
+  }
+`;
 
 const ActualInput = styled.input`
-   display: block;
-   font-size: 16px;
-   border: none;
-   color: inherit;
-   opacity: 1;
-   font-family: inherit;
-   width: 100%;
-`
+  display: block;
+  font-size: 16px;
+  border: none;
+  color: inherit;
+  opacity: 1;
+  font-family: inherit;
+  width: 100%;
+  background-color: transparent;
+`;
